468b7af5ed6d461a33bac409745719bf784ab54b
Tech-A12 <Tech-A12@Tech-A12s-MacBook-Air.local>
2017-06-16 15:56:05 +0100

    Created a fizz buzz test

---
+++ main.js
@@ -0,0 +1,17 @@
+
+num = 0;
+
+for (num = 1; num <= 101; num++) {
+	if ((num % 3 == 0) && (num % 5 == 0)) {
+		console.log('fizzbuzz')
+
+	} else if (num % 5 == 0){
+		console.log('buzz')
+
+	} else if (num % 3 == 0){
+		console.log('fizz')
+
+	} else
+	console.log(num)
+}
+
